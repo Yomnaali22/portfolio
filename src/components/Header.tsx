158a70a68ae6ac3c8e66c5c3ed06f94c1afcb2a7
Yomnaali22 <56122735+Yomnaali22@users.noreply.github.com>
2024-11-02 21:46:28 +0200

    .s

---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+// components/Header.js
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto flex justify-center md:justify-center items-center">
+        {/* Centered Links */}
+        <nav className="hidden md:flex space-x-8">
+          <a href="#contact" className="hover:text-black-300">
+            Contact
+          </a>
+          <a href="#experience" className="hover:text-gray-300">
+            Experience
+          </a>
+          <a href="#projects" className="hover:text-gray-300">
+            Projects
+          </a>
+        </nav>
+
+        {/* Mobile Menu */}
+        <nav className="flex md:hidden space-x-4">
+          <a href="#contact" className="hover:text-gray-300">
+            Contact
+          </a>
+          <a href="#experience" className="hover:text-gray-300">
+            Experience
+          </a>
+          <a href="#projects" className="hover:text-gray-300">
+            Projects
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+}
