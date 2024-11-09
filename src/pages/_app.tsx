0216a5189de47cfef2978e0350da8bfb3ac81803
Yomnaali22 <56122735+Yomnaali22@users.noreply.github.com>
2024-11-09 21:37:47 +0200

    feat: Add theme provider and update styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 export const runtime = "experimental-edge";
-import Header from "@/components/Header";
+import { ThemeProvider } from "@/Theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Header />
+    <ThemeProvider>
       <Component {...pageProps} />;
-    </>
+    </ThemeProvider>
   );
 }
