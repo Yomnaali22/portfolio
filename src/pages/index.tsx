cbba0c703965ec32ca53b6972db2a6e3bdc6580f
Yomnaali22 <56122735+Yomnaali22@users.noreply.github.com>
2024-11-10 10:30:06 +0200

    feat: Implement basic layout and structure for landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,25 @@
+import { useTheme } from "@/Theme";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import IntroSection from "@/components/IntroSection";
+import ExperienceSection from "@/components/ExperienceSection";
+import ProjectsSection from "@/components/ProjectsSection";
+
 export default function Home() {
+  const { theme } = useTheme();
   return (
-    <div>
-      <h1>Home</h1>
+    <div
+      className="font-firacoderegular w-full"
+      style={{
+        backgroundColor: theme.backgroundColor,
+        transition: theme.transition,
+      }}
+    >
+      <Header />
+      <IntroSection />
+      <ExperienceSection />
+      <ProjectsSection />
+      <Footer />
     </div>
   );
 }
