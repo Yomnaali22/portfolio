677fe92b18355fb8ab8be082f73e9e9636e948e1
Yomnaali22 <56122735+Yomnaali22@users.noreply.github.com>
2024-11-10 10:43:19 +0200

    style: Update font weights and sizes in IntroSection component

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -11,9 +11,11 @@ const IntroSection: React.FC = () => {
       }}
     >
       <div className="flex flex-col ml-40">
-        <p className="firacoderegular text-20px">Hi, Yomna here...</p>
-        <h1 className="firacoderegular text-20px">Frontend Developer</h1>
-        <p className="firacoderegular text-20px">Passionate about design, coding and utilizing the AI</p>
+        <p className="font-firacoderegular text-20px">Hi, Yomna here...</p>
+        <h1 className="font-firacodebold text-10px">Frontend Developer</h1>
+        <p className="font-firacoderegular text-20px">
+          Passionate about design, coding and utilizing the AI
+        </p>
       </div>
       <Image
         src="/images/avatarGroup.webp"
