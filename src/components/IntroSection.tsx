a334b4548a3364459db7a372f66f68478efcf7f9
Yomnaali22 <56122735+Yomnaali22@users.noreply.github.com>
2024-11-10 10:41:46 +0200

    feat: Implement intro section UI elements and styles

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -4,11 +4,27 @@ import Image from "next/image";
 const IntroSection: React.FC = () => {
   const { theme } = useTheme();
   return (
-    <div className="flex flex-row">
-      <div className="flex flex-col">
-        <div>Intro</div>
+    <div
+      className="flex flex-row justify-between items-center mt-37"
+      style={{
+        border: "1px solid white",
+      }}
+    >
+      <div className="flex flex-col ml-40">
+        <text>Hi, Yomna here...</text>
+        <h1>Frontend Developer</h1>
+        <text>Passionate about design, coding and utilizing the AI</text>
       </div>
-      <Image src="/vercel.svg" alt="Vercel Logo" width={100} height={24} priority />
+      <Image
+        src="/images/avatarGroup.webp"
+        alt="Vercel Logo"
+        width={500}
+        height={500}
+        style={{
+          objectFit: "contain",
+        }}
+        priority
+      />
     </div>
   );
 };
