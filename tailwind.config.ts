161abc26743d57e3130bdd671a4e4fba4b5f7364
Yomnaali22 <56122735+Yomnaali22@users.noreply.github.com>
2024-11-17 21:11:11 +0200

    Define global tailwind theme and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,14 +6,56 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
+      spacing: {
+        // Ensure that w-10 and h-10 are included
+        "10": "1.6rem", // This should be present
+      },
+      animation: {
+        "move-up": "moveUp 1s ease forwards",
+        "fade-in-out": "infinite fadeInOut 1.5s ease-in-out forwards",
+      },
+      keyframes: {
+        fadeInOut: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(20px)",
+            display: "none",
+          },
+          "50%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+          "100%": {
+            opacity: "0",
+            transform: "translateY(-20px)",
+          },
+        },
+        moveUp: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(50px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
       colors: {
-        backgroundColor: "red",
+        background: "var(--background)",
+        headlinesFontColor: "var(--headlinesFontColor)",
+        sectionFontColor: "var(--sectionFontColor)",
+        hoverColor: "var(--hoverColor)",
+        hoverBackground: "var(--hoverBackground)",
+        hoverText: "var(--hoverText)",
       },
       fontFamily: {
         firacodebold: ["FiraCodeBold", "monospace"],
-        firacoderegular: ["firaCodeRegular", "monospace"],
+        firacoderegular: ["FiraCodeRegular", "monospace"],
+        firacodesemibold: ["FiraCodeSemibold", "monospace"],
         sans: ["Roboto", "Arial", "sans-serif"],
       },
     },
