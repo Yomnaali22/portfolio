3aa3c4a2fc79e3426c71444101847b7797bdf55e
Yomnaali22 <56122735+Yomnaali22@users.noreply.github.com>
2025-04-05 16:51:58 +0200

    Fix deployment issue

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "export",
+  distDir: "out", // or "docs" if using GitHub's docs folder
   images: {
     unoptimized: true,
   },
