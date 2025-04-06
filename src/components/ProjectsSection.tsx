465d453f40b25f100f962fcc9b4605b79cf9a0a8
Yomnaali22 <56122735+Yomnaali22@users.noreply.github.com>
2025-04-06 12:18:04 +0200

    adjust the image name to the correct name

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -23,7 +23,7 @@ const projects = [
     link: "",
   },
   {
-    image: "images/landingpage.webp",
+    image: "images/landingPage.webp",
     id: "4",
     description: "Landing Page",
     link: "https://yomnaali22.github.io/Landing-Page/",
