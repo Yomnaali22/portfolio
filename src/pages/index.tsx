016a6f24e5e9a112dd65efa33621d04f40d15528
Yomnaali22 <56122735+Yomnaali22@users.noreply.github.com>
2025-04-12 16:40:23 +0200

    use aciveSection state instead of state object

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,42 @@
-import { Header, Footer, IntroSection, ExperienceSection } from "@/components";
+import { Header, IntroSection, ExperienceSection } from "@/components";
 import ThemeProvider from "@/useTheme";
 import dynamic from "next/dynamic";
 const ProjectsSection = dynamic(() => import("@/components/ProjectsSection"));
+import { useState } from "react";
+import ScrollButton from "@/components/ScrollButton";
 
 export default function Home() {
+  const [activeSection, setActiveSection] = useState("intro");
+
+  const onClick = (item: string) => {
+    setActiveSection(item);
+  };
+
+  const experienceClass = activeSection === "experience"
+    ? "animate-stack-card-up z-[2]"
+    : "absolute hidden z-[0]";
+  const projectsClass = activeSection === "projects"
+    ? "animate-stack-card-up z-[3]"
+    : "absolute hidden z-[0]";
+  const introClass = activeSection === "intro"
+    ? "animate-intro"
+    : "absolute inset-0 intro-blur";
+
   return (
     <ThemeProvider>
-      <Header />
-      <div className="pt-20 h-full flex flex-col pageSpacing responsive-padding">
-        <IntroSection />
-        <ExperienceSection />
-        <ProjectsSection />
+      <div>
+        <Header onClick={onClick} />
+        <div className={`responsive-padding ${introClass}`}>
+          <IntroSection />
+        </div>
+        <div className={`experienceSec-responsive-padding ${experienceClass}`}>
+          <ExperienceSection />
+        </div>
+        <div className={`projectsSec-responsive-padding ${projectsClass}`}>
+          <ProjectsSection />
+        </div>
+        {activeSection !== "intro" ? <ScrollButton onClick={onClick} /> : null}
       </div>
-      <Footer />
     </ThemeProvider>
   );
 }
